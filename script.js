c89885f5032b8314d37335325c9d4acf6c2490e2
Paolo Micheletti <50237067+Palvoluss@users.noreply.github.com>
2020-12-16 11:47:42 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
+
+
 let btnAdd = document.querySelector("#add");
 let btnSub = document.querySelector("#sub");
 
 let input = document.querySelector("input");
 
 btnAdd.addEventListener("click", () => {
-	input.value = parseInt(input.value) + 1;
+	if (input.value<Math.pow(2,32)) {
+		input.value = parseInt(input.value) + 1;
+	} else {alert("Seriamente?")};
 });
 
 btnSub.addEventListener("click", () => {
-	input.value = parseInt(input.value) - 1;
+	if (input.value>-Math.pow(2,32)) {
+		input.value = parseInt(input.value) - 1;
+	} else {alert("Problemi di insonnia?")}
 });
